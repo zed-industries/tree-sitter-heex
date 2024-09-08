1dd45142fbb05562e35b2040c6129c9bca346592
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-09-08 16:03:52 -0400

    Update Tree-sitter-cli to 0.23

---
+++ Package.swift
@@ -0,0 +1,60 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterHeex",
+    products: [
+        .library(name: "TreeSitterHeex", targets: ["TreeSitterHeex"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+    ],
+    targets: [
+        .target(
+            name: "TreeSitterHeex",
+            dependencies: [],
+            path: ".",
+            exclude: [
+                "Cargo.toml",
+                "Makefile",
+                "binding.gyp",
+                "bindings/c",
+                "bindings/go",
+                "bindings/node",
+                "bindings/python",
+                "bindings/rust",
+                "prebuilds",
+                "grammar.js",
+                "package.json",
+                "package-lock.json",
+                "pyproject.toml",
+                "setup.py",
+                "test",
+                "examples",
+                ".editorconfig",
+                ".github",
+                ".gitignore",
+                ".gitattributes",
+                ".gitmodules",
+            ],
+            sources: [
+                "src/parser.c",
+                // NOTE: if your language has an external scanner, add it here.
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterHeexTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterHeex",
+            ],
+            path: "bindings/swift/TreeSitterHeexTests"
+        )
+    ],
+    cLanguageStandard: .c11
+)
